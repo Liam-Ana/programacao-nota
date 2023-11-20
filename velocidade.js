a79dbe74e3c44df6aacdcd8e92894bf6d6f9e63f
-Ana <126469749+Liam-Ana@users.noreply.github.com>
2023-11-20 08:07:59 -0300

    20/11

--- velocidade.js
+++ velocidade.js
@@ -1,31 +1,22 @@
 const form = document.querySelector("form")
-const resp = document.querySelector("#outResp")
-const resp1 = document.querySelector("#outResp1")
+const resp = document.querySelector("h3")
 
-//ouvinte 
-form.addEventListener("submit", (e)=>{
+//ouvinte
+
+form.addEventListener("submit", (e) =>{
     e.preventDefault()
 
-    const ladoA = Number(form.inLadoA.value)
-    const ladoB = Number(form.inLadoB.value)
-    const ladoC = Number(form.inLadoC.value)
+    const permitida = Number(form.inPermitida.value)
+    const condutor = Number(form.inCondutor.value)
+    let leve = permitida + (permitida * 0.2)
 
-    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB)){
-        alert("Essas medidas não podem formar um triângulo. Um dos lados não pode ser maior que a soma dos outros dois.")
-        form.inLadoA.focus() 
-        form.inLadoB.focus()
-        form.inLadoC.focus()
-        return
-    }
+    if(condutor <= permitida){
+        resp.innerText = `Situação: Sem Multa`
 
-    resp.innerText = `Lados podem formar um triângulo ` 
+    }else if(condutor <= leve) {
+        resp.innerText = `Situação: Multa Leve`
 
-    if(ladoA == ladoB && ladoA == ladoC){
-        resp1.innerText = `Tipo: Equilátero `
-    } else if((ladoA == ladoB )|| (ladoA == ladoC) || (ladoB == ladoC)){
-        resp1.innerText = `Tipo: Isóceles`
     }else{
-        resp1.innerText = `Tipo: Escaleno`
+        resp.innerText = `Situação: Multa Grave`
     }
-
 })
